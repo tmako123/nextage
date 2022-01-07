9eb94bce919dc2d8c398de8d70df863880c25710
tmako123 <tomioka_makoto@yahoo.co.jp>
2022-01-08 00:25:58 +0900

    add g2o CubicFunction

--- catchup/g2o/CubicFunction/main.cpp
+++ catchup/g2o/CubicFunction/main.cpp
@@ -1,5 +1,5 @@
 /*******************************************************
- * Copyright(c) 2018-2022, tmako123
+ * Copyright(c) 2018, tmako123
  * All rights reserved.
  *
  * This file is distributed under the GNU Lesser General Public License v3.0.
@@ -19,8 +19,8 @@
 #include "g2o/core/sparse_optimizer.h"
 #include "g2o/solvers/eigen/linear_solver_eigen.h"
 
-//the params, a and b for ax + b
-class VertexParams : public g2o::BaseVertex<2, Eigen::Vector2d> {
+//the params, a and b for ax^3 + bx^2 + cx + d
+class VertexParams : public g2o::BaseVertex<4, Eigen::Vector4d> {
 public:
     VertexParams() {}
 
@@ -29,15 +29,15 @@ public:
     virtual void setToOriginImpl() {}
     virtual void oplusImpl(const double* update)
     {
-        Eigen::Vector2d::ConstMapType v(update);
+        Eigen::Vector4d::ConstMapType v(update);
         _estimate += v;
     }
 };
 
-class EdgePointOnLinearFunction
+class EdgePointOnCubicFunction
     : public g2o::BaseUnaryEdge<1, Eigen::Vector2d, VertexParams> {
 public:
-    EdgePointOnLinearFunction() {}
+    EdgePointOnCubicFunction() {}
     virtual bool read(std::istream& /*is*/) { return false; }
     virtual bool write(std::ostream& /*os*/) const { return false; }
     void computeError()
@@ -45,8 +45,12 @@ public:
         const VertexParams* vertices = static_cast<const VertexParams*>(_vertices[0]);
         const double a = vertices->estimate()[0];
         const double b = vertices->estimate()[1];
+        const double c = vertices->estimate()[2];
+        const double d = vertices->estimate()[3];
         const double x = measurement()[0];
-        const double y = a * x + b;
+        const double x2 = x * x;
+        const double x3 = x2 * x;
+        const double y = a * x3 + b * x2 + c * x + d;
         _error[0] = y - measurement()[1];
     }
 };
@@ -56,9 +60,9 @@ int main(int argc, char** argv)
     int maxIterations = 10;
     bool verbose = false;
 
-    // problem y = 0.5 x + 1
-    double a = 0.5, b = 1.0; //ground truth
-    double a_ = 0.0, b_ = 0.0; //initial value
+    // problem y = 0.5x^3 - x^2 -0.5x + 1
+    double a = 0.5, b = -1.0, c = -0.5, d = 1.0; //ground truth
+    double a_ = 0.0, b_ = 0.0, c_ = 0.0, d_ = 0.0; //initial value
     double mu = 0., sigma = 0.3; // measurement variance
 
     // create masurements
@@ -72,7 +76,9 @@ int main(int argc, char** argv)
 
     for (int i = 0; i < numX; i++) {
         double x = minX + resoX * i;
-        double y = a * x + b;
+        double x2 = x * x;
+        double x3 = x * x2;
+        double y = a * x3 + b * x2 + c * x + d;
         xy.push_back(Eigen::Vector2d(x, y));
         xy_.push_back(Eigen::Vector2d(x, y + dist(engine)));
     }
@@ -90,12 +96,12 @@ int main(int argc, char** argv)
     // 1. add the parameter vertex
     VertexParams* params = new VertexParams();
     params->setId(0);
-    params->setEstimate(Eigen::Vector2d(a_, b_));
+    params->setEstimate(Eigen::Vector4d(a_, b_, c_, d_));
     optimizer.addVertex(params);
 
     // 2. add the measured to be on the linear function
     for (int i = 0; i < xy_.size(); ++i) {
-        EdgePointOnLinearFunction* e = new EdgePointOnLinearFunction;
+        EdgePointOnCubicFunction* e = new EdgePointOnCubicFunction;
         e->setInformation(Eigen::Matrix<double, 1, 1>::Identity());
         e->setVertex(0, params);
         e->setMeasurement(xy_[i]);
@@ -113,9 +119,11 @@ int main(int argc, char** argv)
 
     // print out the result
     std::cout << "Target Function" << std::endl;
-    std::cout << "ax + b" << std::endl;
+    std::cout << "ax + bax^3 + bx^2 + cx + d" << std::endl;
     std::cout << "Iterative least squares solution" << std::endl;
     std::cout << "a  = " << a_ << " -> " << params->estimate()(0) << " for " << a << std::endl;
     std::cout << "b  = " << b_ << " -> " << params->estimate()(1) << " for " << b << std::endl;
+    std::cout << "c  = " << c_ << " -> " << params->estimate()(2) << " for " << c << std::endl;
+    std::cout << "d  = " << d_ << " -> " << params->estimate()(3) << " for " << d << std::endl;
     return 0;
 }
